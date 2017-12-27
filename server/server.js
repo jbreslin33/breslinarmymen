bc40df598f8274842bf70e57465c2343e55c10d9
Jim Breslin <jbreslin@gmail.com>
2017-12-26 20:19:53 -0500

    promise is working

--- server/server.js
+++ server/server.js
@@ -29,7 +29,20 @@ initialize: function()
         //a local queue of messages we delay if faking latency
     	this.messages = [];
 
+
 	this.createGame();
+
+var pgp = require('pg-promise')(/*options*/)
+//var db = pgp('postgres://username:password@host:port/database')
+var db = pgp('postgres://postgres:mibesfat@localhost:5432/jamesanthonybreslin')
+
+db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
 },
 
 log: function() 
